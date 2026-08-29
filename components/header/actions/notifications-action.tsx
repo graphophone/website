3620aa0b8fa18b802b6caedd720be0d6b6c98b95
justkinou <justkinou@proton.me>
2026-08-29 20:44:50 +0200

    feat: add notifications pagination

--- components/header/actions/notifications-action.tsx
+++ components/header/actions/notifications-action.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { BellSimple } from 'phosphor-react';
-import { useState } from 'react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
 
 function NotificationsAction() {
-    const notifications = Array.from({ length: 10 }).map((_, i) => (
+    const notifications = Array.from({ length: 32 }).map((_, i) => (
         {
             title: `Notification ${i}`,
             description: "Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
@@ -14,10 +15,33 @@ function NotificationsAction() {
     ));
     const [isNotificationsHovered, setIsNotificationsHovered] = useState(false);
 
+    const pageSize = 3;
+    const pagesCount = Math.ceil(notifications.length / pageSize);
+    const [nearestPages, setNearestPages] = useState<number[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        const newNearestPages = [];
+        if (currentPage === pagesCount && currentPage > 2) {
+            newNearestPages.push(currentPage - 2);
+        }
+        if (currentPage > 1) {
+            newNearestPages.push(currentPage - 1);
+        }
+        newNearestPages.push(currentPage);
+        if (currentPage < pagesCount) {
+            newNearestPages.push(currentPage + 1);
+        }
+        if (currentPage === 1 && currentPage < pagesCount - 1) {
+            newNearestPages.push(currentPage + 2);
+        }
+        setNearestPages(newNearestPages);
+    }, [currentPage])
+
     return (
         <div className="w-full h-full flex justify-center items-center">
-            <DropdownMenu>
-                <DropdownMenuTrigger render={
+            <Popover>
+                <PopoverTrigger render={
                     <Button
                         variant="link"
                         onMouseEnter={() => { setIsNotificationsHovered(true) }}
@@ -37,17 +61,52 @@ function NotificationsAction() {
                         }
                     </Button>
                 } />
-                <DropdownMenuContent className="w-60" align="end">
-                    <DropdownMenuGroup>
-                        {notifications.map((notification, i) => (
-                            <DropdownMenuItem key={i} className="cursor-pointer flex flex-col items-baseline gap-1">
-                                <b className="w-[70%] text-ellipsis ">{notification.title}</b>
+                <PopoverContent className="w-70 p-1 rounded-[6px] flex-col gap-1" align="end">
+                    <div className="px-2 pt-2 text-[18px] font-bold">Notifications</div>
+
+                    <div>
+                        {notifications.slice((currentPage - 1) * pageSize, currentPage * pageSize)
+                            .map((notification, i) => (
+                            <div key={i} className="p-2 rounded-[6px] cursor-pointer flex flex-col items-baseline gap-1 hover:bg-gray-100 transition-all">
+                                <span className="font-semibold max-w-[50%] text-ellipsis">{notification.title}</span>
                                 <div className="text-ellipsis line-clamp-2">{notification.description}</div>
-                            </DropdownMenuItem>
+                            </div>
                         ))}
-                    </DropdownMenuGroup>
-                </DropdownMenuContent>
-            </DropdownMenu>
+                    </div>
+
+                    <Pagination>
+                        <PaginationContent>
+                            {currentPage === 1 ? <></> :
+                                <PaginationItem>
+                                    <PaginationPrevious
+                                        className="cursor-pointer"
+                                        onClick={() => setCurrentPage(prev => prev - 1)}
+                                    />
+                                </PaginationItem>
+                            }
+                            { nearestPages.map(pageNumber => (
+                                <PaginationItem key={pageNumber}>
+                                    <PaginationLink
+                                        isActive={pageNumber === currentPage}
+                                        className="cursor-pointer"
+                                        onClick={() => setCurrentPage(pageNumber)}
+                                    >
+                                        { pageNumber }
+                                    </PaginationLink>
+                                </PaginationItem>
+                            )) }
+                            {currentPage === pagesCount ? <></> :
+                                <PaginationItem>
+                                    <PaginationNext
+                                        className="cursor-pointer"
+                                        onClick={() => setCurrentPage(prev => prev + 1)}
+                                    />
+                                </PaginationItem>
+                            }
+                        </PaginationContent>
+                    </Pagination>
+                </PopoverContent>
+            </Popover>
         </div>
     )
 }
